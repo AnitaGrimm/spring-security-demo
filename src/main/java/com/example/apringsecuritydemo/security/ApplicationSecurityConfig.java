934765c94fbf08c85f0ffc86543f14e17c729ba0
AnitaGrimm <ZamaletdinovaAnna@gmail.com>
2022-05-22 17:11:34 +0300

    In-memory auth with remember-me and authorities

---
+++ src/main/java/com/example/apringsecuritydemo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,95 @@
+package com.example.apringsecuritydemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.example.apringsecuritydemo.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() //TODO: disable this only on non-brousers clients, like APIs
+                // Cross Site Request Forgery
+                .authorizeRequests()
+                .antMatchers("/", "index","/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                Equivalent of PreAuthorize
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSES_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSES_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSES_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21)) //defaults for 2 weeks
+                    .key("somethingverysecured")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    // If csrf enabled(default), you should delete this line
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+         UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                 .authorities(STUDENT.getGrantedAuthorities())
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .build();
+         UserDetails lindaUser = User.builder()
+                 .username("linda")
+                 .password(passwordEncoder.encode("password123"))
+//                 .roles(ADMIN.name()) // ROLE_ADMIN
+                 .authorities(ADMIN.getGrantedAuthorities())
+                 .build();
+         UserDetails tomUser = User.builder()
+                 .username("tom")
+                 .password(passwordEncoder.encode("password123"))
+//                 .roles(ADMINTRAINEE.name()) // ROLE_ADMIN
+                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                 .build();
+
+         return new InMemoryUserDetailsManager(
+                 annaSmithUser,
+                 lindaUser,
+                 tomUser
+         );
+    }
+}
